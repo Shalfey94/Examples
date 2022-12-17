d8e0fe0e20bdc0d30f52203c188db5da3a251949
SergeyKharkovskiy <shalfey.90@mail.ru>
2022-12-17 13:36:11 +0300

    задача8

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 23;
+int b1 = 12;
+int c1 = 53;
+int a2 = 35;
+int b2 = 2;
+int c2 = 15;
+int a3 = 66;
+int b3 = 8;
+int c3 = 12;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
+

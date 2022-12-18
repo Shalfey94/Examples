58b5537df24e32ba1afe559603bc149fecfe4cd8
SergeyKharkovskiy <shalfey.90@mail.ru>
2022-12-18 17:57:39 +0300

    задача10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {11, 33, 2, 43, 12, 9, 24, 12};
+
+int n = array.Length;
+int find = 12;
+int index = 0;
+
+while(index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
